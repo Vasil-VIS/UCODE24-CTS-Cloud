85949c2c687fb68c7f7ed81cbb0a0d91a4e9f28e
Vasil-VIS <44656749+Vasil-VIS@users.noreply.github.com>
2024-06-14 09:59:08 +0000

    update plot ui

--- app.py
+++ app.py
@@ -20,19 +20,22 @@ ROOT = entity_folder_path
 
 #Parametrization is used for user input (left side of the screen)
 class Parametrization(ViktorParametrization):
-    page_1 = Page('Cable Tension Calculator')
+    page_1 = Page('Cable Tension Calculator', views=['get_plotly_view'])
     page_1.field_1 = Text("""# Welcome to Cable Tension Calculator!""")
     page_1.field_2 = Text("""### This app is designed to automate calculating tension on big cables!""")
     page_1.lb1 = LineBreak()
-    page_1.span = NumberField('1.Input the span lenght',flex=100,min=0)
+    page_1.span = NumberField('1.Input the span lenght',flex=100,min=0, default=300)
     page_1.lb2 = LineBreak()
-    page_1.weight = NumberField('2.Input the weight per unit lenght',flex=100,min=0)
+    page_1.weight = NumberField('2.Input the weight per unit lenght',flex=100,min=0, default=15.97)
     page_1.lb3 = LineBreak()
-    page_1.cantenery = NumberField('3.Input the contenary constant',flex=100,min=0)
+    page_1.cantenery = NumberField('3.Input the contenary constant',flex=100,min=0, default=1753.287)
     page_1.lb4 = LineBreak()
-    page_1.height = NumberField('4.Input the height lenght',flex=100,min=0)
+    page_1.height = NumberField('4.Input the height lenght',flex=100,min=0, default=50)
     page_1.lb4 = LineBreak()
-    page_1.download_btn = DownloadButton('Download Report',flex=30,longpoll=True, method='download_file')    
+    page_1.download_btn = DownloadButton('Download Report',flex=30,longpoll=True, method='download_file')
+
+    page_2 = Page('Ice Load')
+    Page_3 = Page('Wind Load')
 
 
 class Controller(ViktorController):
@@ -55,8 +58,6 @@ class Controller(ViktorController):
 
         xlsx_value = buffer.getvalue()
 
-        graph = get_graph(table, cantenery, weight)
-
         return DownloadResult(File.from_data(xlsx_value), f'output.xlsx')
 
     @PlotlyView("CTC Plot", duration_guess=1)
@@ -65,35 +66,6 @@ class Controller(ViktorController):
         weight=params.page_1.weight
         cantenery=params.page_1.cantenery
         height=params.page_1.height
-        fig=get_fig(span,weight,cantenery)
-        return PlotlyResult(fig)
-    # def get_user_manual(self, params, **kwargs):
-    #     print(entity_folder_path)
-    #     file_path = entity_folder_path + '/cable.pdf'
-    #     return DownloadResult(file_content=File.from_path(file_path), file_name='cable.pdf')
-
-
-    # @ImageAndDataView("Image View", duration_guess=1)
-    # def get_image_data_view(self, params, **kwargs):
-    #     span=params.page_1.span
-    #     weight=params.page_1.weight
-    #     cantenery=params.page_1.cantenery
-    #     height=params.page_1.height
-    #     violations = []
-    #     violations.append(InputViolation("Input cannot be empty!", fields=['span', 'weight', 'cantenery','height']))
-    #     if violations:
-    #         raise UserError("Cannot calculate!", input_violations=violations)
-        
-    #     """
-        
-    #     PLOT
-        
-        
-        
-    #     """
-
-    #     svg_data = StringIO()
-    #     fig.savefig(svg_data, format='svg')
-    #     plt.close()
-
-    #     return ImageAndDataResult(svg_data, data_group)
+        table = get_table(span,weight,cantenery,height)
+        fig = get_graph(table, cantenery, weight)
+        return PlotlyResult(fig.to_json())
